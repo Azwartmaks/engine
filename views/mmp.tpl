1ce4abd4d6e9e9441560b17c27461a6011fcdbaf
Azwartmaks <azwart@mail.ru>
2015-05-05 18:06:57 +0300

    changes

--- views/mmp.tpl
+++ views/mmp.tpl
@@ -6,19 +6,13 @@
         {include file="blocks/breadcrumbs_mmp.tpl"}
         <hr/>
         <h1>{$header}</h1>
-        {$text}
-        <ul>
-            {foreach $years as $year}
-                <li><a href="{$year.year}-{$make}-{$model}-{$productUrl}.html">{$year.year} {$make} {$model} {$product}</a></li>
-            {/foreach}
-        </ul>
-        
-        <h3>Other {$make} models</h3>
-        <ul>
-            {foreach $models as $model}
-            <li><a href="{$make}-{$model.model}-{$productUrl}.html">{$model.model} {$productname}</a></li>
-            {/foreach}
-        </ul>
+{*        {$text}*}
+        <p>If we speak of performance cars, it is no longer new that every car enthusiast is obsessed with their luxury or sports cars with installed custom grilles.</p>
+        <p>If we speak of performance cars, it is no longer new that every car enthusiast is obsessed with their luxury or sports cars. If this is so, those car owners love their auto to be the finest of all when being compared from others. It is a guy thing, as everybody knows of this trait, when it comes to cars, they want something that they can boost it out. Because of the desires of those car lovers that theirs will be astounding, this ascends the emerged of customized and modifications of cars. Moreover, since the potential of this investment, market is feasible, more and more auto companies dive in with the car trends and began to invent gears for this purpose. Hence, if you are looking for performance parts of your <b>{$make|capitalize} {$model|capitalize}</b>, they are available even online and you can certainly find anything you wish for without hassle.</p>
+	<p>With the endless popularity of this trend, even the purpose of having a car has changed. At first, it was of necessity, but today’s era, it is now part of a hobby. Car companies of any car makes and models of course have thought of it that’s why they had came up with customizations that can absolutely keep with uniqueness, versatility or simply ensembles the car owner’s personality and style.</p>
+	<p>People who may not be aware of this {$make|capitalize} {$model|capitalize} trend will point their fingers as the culprit of this modification madness. But, do they really know how this trend started? Car owners of {$make|capitalize} {$model|capitalize} joined  hands together yearly and discuss on how this car makes and models perform well. They meet up not just to discuss the modifications on the appearance of the car, but likewise deal with the modifications they can make on the mechanical operation. Those {$make|capitalize} {$model|capitalize} owners absolutely want theirs to appear exceptional and fastest ever imagined.</p>
+	<p><b>{$make|capitalize} {$model|capitalize}</b> performance accessories never failed its car enthusiasts to supply only the best. It only wants a level-up performance to its car models. More so, it wanted its car owners to get satisfied and impressed with superb engine system performance. They keep the over-all parts to be performing well without fail. The reason for keeping each {$make|capitalize} {$model|capitalize} is superior in every part is due to the fact that when one part fails, the entire mechanical functionality also fails. Therefore, <b>{$model|capitalize}</b> and other models performance accessories are certainly designed to enhance and upgrade the driving dynamics as well the car’s looks and style. For example, <b>{$make|capitalize}</b>  <a href="http://www.scienceg8.com/abs-plastic-body-kits.html">body kits</a> are for purposes to put style on the car’s body. Such body kits include ground effects, wings or spoilers, body panel accessories and skirts, and fender flares.</p>
+	<p>In addition, {$make|capitalize} {$model|capitalize} performance parts is spoiler or wings, such are designed for purposes of style and function of the car. More so, it upgrades the speed of the car and thus best for sports and racing cars.</p>
     </div>
     <div class="sidebar col-sm-3">
         <div class="side-block first">
@@ -37,7 +31,27 @@
                 {/foreach}
             </ul>
         </div>
-        <div class="side-block first">
+        <div class="side-block">
+            <div class="side-h2">
+                Choose year for your {$make|capitalize} {$model|capitalize}
+            </div>
+            <ul class="side-list">
+                {foreach $years as $year}
+                    <li><a href="{$year.year}-{$make}-{$model}-{$productUrl}.html">{$year.year} {$make} {$model} {$product}</a></li>
+                {/foreach}
+            </ul>
+        </div> 
+        <div class="side-block">
+            <div class="side-h2">
+                Choose other model
+            </div>
+            <ul class="side-list">
+                {foreach $models as $model}
+                <li><a href="{$make}-{$model.model}-{$productUrl}.html">{$model.model} {$productname}</a></li>
+                {/foreach}
+            </ul>
+        </div> 
+        <div class="side-block">
             <div class="side-h2">
                 Videos
             </div>
--- views_c/988ac85e5e91c0f1a1facbfa1ead7c1c71036ed2.file.mmp.tpl.php
+++ views_c/988ac85e5e91c0f1a1facbfa1ead7c1c71036ed2.file.mmp.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.21-dev, created on 2015-04-27 15:20:25
+<?php /* Smarty version Smarty-3.1.21-dev, created on 2015-05-05 18:03:38
          compiled from "views\mmp.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:25832553dfbb360cfd8-75363387%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '988ac85e5e91c0f1a1facbfa1ead7c1c71036ed2' => 
     array (
       0 => 'views\\mmp.tpl',
-      1 => 1430137219,
+      1 => 1430838208,
       2 => 'file',
     ),
   ),
@@ -20,16 +20,15 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'variables' => 
   array (
     'header' => 0,
-    'text' => 0,
-    'years' => 0,
-    'year' => 0,
     'make' => 0,
     'model' => 0,
-    'productUrl' => 0,
+    'products' => 0,
     'product' => 0,
+    'years' => 0,
+    'year' => 0,
+    'productUrl' => 0,
     'models' => 0,
     'productname' => 0,
-    'products' => 0,
   ),
   'has_nocache_code' => false,
 ),false); /*/%%SmartyHeaderCode%%*/?>
@@ -44,42 +43,29 @@ $_valid = $_smarty_tpl->decodeProperties(array (
         <hr/>
         <h1><?php echo $_smarty_tpl->tpl_vars['header']->value;?>
 </h1>
-        <?php echo $_smarty_tpl->tpl_vars['text']->value;?>
 
-        <ul>
-            <?php  $_smarty_tpl->tpl_vars['year'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['year']->_loop = false;
- $_from = $_smarty_tpl->tpl_vars['years']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
-foreach ($_from as $_smarty_tpl->tpl_vars['year']->key => $_smarty_tpl->tpl_vars['year']->value) {
-$_smarty_tpl->tpl_vars['year']->_loop = true;
-?>
-                <li><a href="<?php echo $_smarty_tpl->tpl_vars['year']->value['year'];?>
--<?php echo $_smarty_tpl->tpl_vars['make']->value;?>
--<?php echo $_smarty_tpl->tpl_vars['model']->value;?>
--<?php echo $_smarty_tpl->tpl_vars['productUrl']->value;?>
-.html"><?php echo $_smarty_tpl->tpl_vars['year']->value['year'];?>
- <?php echo $_smarty_tpl->tpl_vars['make']->value;?>
- <?php echo $_smarty_tpl->tpl_vars['model']->value;?>
- <?php echo $_smarty_tpl->tpl_vars['product']->value;?>
-</a></li>
-            <?php } ?>
-        </ul>
-        
-        <h3>Other <?php echo $_smarty_tpl->tpl_vars['make']->value;?>
- models</h3>
-        <ul>
-            <?php  $_smarty_tpl->tpl_vars['model'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['model']->_loop = false;
- $_from = $_smarty_tpl->tpl_vars['models']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
-foreach ($_from as $_smarty_tpl->tpl_vars['model']->key => $_smarty_tpl->tpl_vars['model']->value) {
-$_smarty_tpl->tpl_vars['model']->_loop = true;
-?>
-            <li><a href="<?php echo $_smarty_tpl->tpl_vars['make']->value;?>
--<?php echo $_smarty_tpl->tpl_vars['model']->value['model'];?>
--<?php echo $_smarty_tpl->tpl_vars['productUrl']->value;?>
-.html"><?php echo $_smarty_tpl->tpl_vars['model']->value['model'];?>
- <?php echo $_smarty_tpl->tpl_vars['productname']->value;?>
-</a></li>
-            <?php } ?>
-        </ul>
+        <p>If we speak of performance cars, it is no longer new that every car enthusiast is obsessed with their luxury or sports cars with installed custom grilles.</p>
+        <p>If we speak of performance cars, it is no longer new that every car enthusiast is obsessed with their luxury or sports cars. If this is so, those car owners love their auto to be the finest of all when being compared from others. It is a guy thing, as everybody knows of this trait, when it comes to cars, they want something that they can boost it out. Because of the desires of those car lovers that theirs will be astounding, this ascends the emerged of customized and modifications of cars. Moreover, since the potential of this investment, market is feasible, more and more auto companies dive in with the car trends and began to invent gears for this purpose. Hence, if you are looking for performance parts of your <b><?php echo smarty_modifier_capitalize($_smarty_tpl->tpl_vars['make']->value);?>
+ <?php echo smarty_modifier_capitalize($_smarty_tpl->tpl_vars['model']->value);?>
+</b>, they are available even online and you can certainly find anything you wish for without hassle.</p>
+	<p>With the endless popularity of this trend, even the purpose of having a car has changed. At first, it was of necessity, but today’s era, it is now part of a hobby. Car companies of any car makes and models of course have thought of it that’s why they had came up with customizations that can absolutely keep with uniqueness, versatility or simply ensembles the car owner’s personality and style.</p>
+	<p>People who may not be aware of this <?php echo smarty_modifier_capitalize($_smarty_tpl->tpl_vars['make']->value);?>
+ <?php echo smarty_modifier_capitalize($_smarty_tpl->tpl_vars['model']->value);?>
+ trend will point their fingers as the culprit of this modification madness. But, do they really know how this trend started? Car owners of <?php echo smarty_modifier_capitalize($_smarty_tpl->tpl_vars['make']->value);?>
+ <?php echo smarty_modifier_capitalize($_smarty_tpl->tpl_vars['model']->value);?>
+ joined  hands together yearly and discuss on how this car makes and models perform well. They meet up not just to discuss the modifications on the appearance of the car, but likewise deal with the modifications they can make on the mechanical operation. Those <?php echo smarty_modifier_capitalize($_smarty_tpl->tpl_vars['make']->value);?>
+ <?php echo smarty_modifier_capitalize($_smarty_tpl->tpl_vars['model']->value);?>
+ owners absolutely want theirs to appear exceptional and fastest ever imagined.</p>
+	<p><b><?php echo smarty_modifier_capitalize($_smarty_tpl->tpl_vars['make']->value);?>
+ <?php echo smarty_modifier_capitalize($_smarty_tpl->tpl_vars['model']->value);?>
+</b> performance accessories never failed its car enthusiasts to supply only the best. It only wants a level-up performance to its car models. More so, it wanted its car owners to get satisfied and impressed with superb engine system performance. They keep the over-all parts to be performing well without fail. The reason for keeping each <?php echo smarty_modifier_capitalize($_smarty_tpl->tpl_vars['make']->value);?>
+ <?php echo smarty_modifier_capitalize($_smarty_tpl->tpl_vars['model']->value);?>
+ is superior in every part is due to the fact that when one part fails, the entire mechanical functionality also fails. Therefore, <b><?php echo smarty_modifier_capitalize($_smarty_tpl->tpl_vars['model']->value);?>
+</b> and other models performance accessories are certainly designed to enhance and upgrade the driving dynamics as well the car’s looks and style. For example, <b><?php echo smarty_modifier_capitalize($_smarty_tpl->tpl_vars['make']->value);?>
+</b>  <a href="http://www.scienceg8.com/abs-plastic-body-kits.html">body kits</a> are for purposes to put style on the car’s body. Such body kits include ground effects, wings or spoilers, body panel accessories and skirts, and fender flares.</p>
+	<p>In addition, <?php echo smarty_modifier_capitalize($_smarty_tpl->tpl_vars['make']->value);?>
+ <?php echo smarty_modifier_capitalize($_smarty_tpl->tpl_vars['model']->value);?>
+ performance parts is spoiler or wings, such are designed for purposes of style and function of the car. More so, it upgrades the speed of the car and thus best for sports and racing cars.</p>
     </div>
     <div class="sidebar col-sm-3">
         <div class="side-block first">
@@ -104,7 +90,50 @@ $_smarty_tpl->tpl_vars['product']->_loop = true;
                 <?php } ?>
             </ul>
         </div>
-        <div class="side-block first">
+        <div class="side-block">
+            <div class="side-h2">
+                Choose year for your <?php echo smarty_modifier_capitalize($_smarty_tpl->tpl_vars['make']->value);?>
+ <?php echo smarty_modifier_capitalize($_smarty_tpl->tpl_vars['model']->value);?>
+
+            </div>
+            <ul class="side-list">
+                <?php  $_smarty_tpl->tpl_vars['year'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['year']->_loop = false;
+ $_from = $_smarty_tpl->tpl_vars['years']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
+foreach ($_from as $_smarty_tpl->tpl_vars['year']->key => $_smarty_tpl->tpl_vars['year']->value) {
+$_smarty_tpl->tpl_vars['year']->_loop = true;
+?>
+                    <li><a href="<?php echo $_smarty_tpl->tpl_vars['year']->value['year'];?>
+-<?php echo $_smarty_tpl->tpl_vars['make']->value;?>
+-<?php echo $_smarty_tpl->tpl_vars['model']->value;?>
+-<?php echo $_smarty_tpl->tpl_vars['productUrl']->value;?>
+.html"><?php echo $_smarty_tpl->tpl_vars['year']->value['year'];?>
+ <?php echo $_smarty_tpl->tpl_vars['make']->value;?>
+ <?php echo $_smarty_tpl->tpl_vars['model']->value;?>
+ <?php echo $_smarty_tpl->tpl_vars['product']->value;?>
+</a></li>
+                <?php } ?>
+            </ul>
+        </div> 
+        <div class="side-block">
+            <div class="side-h2">
+                Choose other model
+            </div>
+            <ul class="side-list">
+                <?php  $_smarty_tpl->tpl_vars['model'] = new Smarty_Variable; $_smarty_tpl->tpl_vars['model']->_loop = false;
+ $_from = $_smarty_tpl->tpl_vars['models']->value; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array');}
+foreach ($_from as $_smarty_tpl->tpl_vars['model']->key => $_smarty_tpl->tpl_vars['model']->value) {
+$_smarty_tpl->tpl_vars['model']->_loop = true;
+?>
+                <li><a href="<?php echo $_smarty_tpl->tpl_vars['make']->value;?>
+-<?php echo $_smarty_tpl->tpl_vars['model']->value['model'];?>
+-<?php echo $_smarty_tpl->tpl_vars['productUrl']->value;?>
+.html"><?php echo $_smarty_tpl->tpl_vars['model']->value['model'];?>
+ <?php echo $_smarty_tpl->tpl_vars['productname']->value;?>
+</a></li>
+                <?php } ?>
+            </ul>
+        </div> 
+        <div class="side-block">
             <div class="side-h2">
                 Videos
             </div>
